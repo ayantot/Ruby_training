90ef5ce7901e957eb6a10e4b2071c28bff84976f
TEGNY Thomas <thomas.tegny06@gmail.com>
2020-01-08 11:12:53 +0100

    initialize jquery to rails v6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap");
+require("bootstrap")
+require("posts/style.js")
+require("jquery")
 
 //= require jquery3
 //= require popper
